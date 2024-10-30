13db4da87f482513121dbea9540d967b124279e3
Fabian Sanchez <fabiansanchez@sheridan.edu>
2024-10-29 20:57:39 -0600

    Input Binding Changes

--- ch03/src/app/products/product-detail/product-detail.component.ts
+++ ch03/src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ViewEncapsulation, ChangeDetectionStrategy, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,7 +7,7 @@ import { Component, Input, Output, EventEmitter, ViewEncapsulation, ChangeDetect
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges {
   @Input() name = '';
   
   @Output() bought = new EventEmitter();
@@ -18,5 +18,21 @@ export class ProductDetailComponent {
   get productName(): string { 
     console.log(`Get ${this.name}`);
     return this.name;
+  }
+
+  constructor() {
+    console.log(`Name is ${this.name} in the constructor`);
+  }
+  ngOnInit(): void {
+    console.log(`Name is ${this.name} in the ngOnInit`)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const product = changes['name'];
+    if (!product.isFirstChange()) {
+      const oldValue = product.previousValue;
+      const newValue = product.currentValue;
+      console.log(`Product changed from ${oldValue} to ${newValue}`)
+    }
   }
 }
--- ch03/src/app/products/product-list/product-list.component.ts
+++ ch03/src/app/products/product-list/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  selectedProduct = '';
+  selectedProduct = 'Microphone';
 
   onBuy() {
     window.alert(`You just bought ${this.selectedProduct}!`)
